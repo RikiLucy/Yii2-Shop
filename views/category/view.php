ed6b9a138d772848c17615f81b590657949295a6
RikiLucy <riki.and.lucy@gmail.com>
2017-01-18 19:01:00 +0700

    set metatags and Category->Name

--- views/category/view.php
+++ views/category/view.php
@@ -2,7 +2,6 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
 use yii\helpers\Html;
 
 ?>
@@ -54,7 +53,7 @@ use yii\helpers\Html;
 
             <div class="col-sm-9 padding-right">
                 <div class="features_items"><!--features_items-->
-                    <h2 class="title text-center">Features Items</h2>
+                    <h2 class="title text-center"><?=$category->name ?></h2>
                     <?php if(!empty($products)): ?>
                         <?php $i = 0; foreach ($products as $product): ?>
                     <div class="col-sm-4">
