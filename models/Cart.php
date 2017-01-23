8c9c0e286311108c4d60fba90f9002ed6f9740d5
RikiLucy <riki.and.lucy@gmail.com>
2017-01-23 15:24:04 +0700

    func addToCart

--- models/Cart.php
+++ models/Cart.php
@@ -9,9 +9,31 @@
 namespace app\models;
 
 
-use yii\base\Model;
+use yii\db\ActiveRecord;
 
-class Cart extends Model
+class Cart extends ActiveRecord
 {
+    public function addToCart($product, $qty = 1){
+        if(isset($_SESSION['cart'][$product->id])){ //если товар есть, увеличиваем его кол-во
+            $_SESSION['cart'][$product->id]['qty'] += $qty;
+        }else{
+            $_SESSION['cart'][$product->id] = [ // если товара нету - создаем
+                'qty' => $qty,
+                'name' => $product->name,
+                'price' => $product->price,
+                'img' => $product->img
+            ];
+        }
+        $_SESSION['cart.qty'] = isset($_SESSION['cart.qty']) ? $_SESSION['cart.qty'] + $qty : $qty;
+        // если товар уже был, то его кол-во плюс еще, если не было, передаем просто кол-во
+        $_SESSION['cart.sum'] = isset($_SESSION['cart.sum']) ? $_SESSION['cart.sum'] + $qty * $product->price : $qty * $product->price;
+
+
+
+
+
+
+
+    }
 
 }
