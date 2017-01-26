be66867bf670582b4c279275d95679a8ed52493c
RikiLucy <riki.and.lucy@gmail.com>
2017-01-26 14:15:03 +0700

    оформление заказа

--- config/web.php
+++ config/web.php
@@ -29,6 +29,14 @@ $config = [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
+            /*'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'localhost',
+                'username' => 'username',
+                'password' => 'password',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],*/
             'useFileTransport' => true,
         ],
         'log' => [
