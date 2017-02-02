89e904d2b8f88c099d9166df3c8d32f22914e8e3
RikiLucy <riki.and.lucy@gmail.com>
2017-02-02 12:58:37 +0700

    Модуль для админа

--- modules/admin/controllers/AppAdminController.php
+++ modules/admin/controllers/AppAdminController.php
@@ -8,25 +8,9 @@
 
 namespace app\modules\admin\controllers;
 use yii\base\Controller;
-use yii\filters\AccessControl;
 
 class AppAdminController extends Controller
 {
-    public function behaviors()
-    {
-        return ['access' => [
-            'class' => AccessControl::className(),
-            'rules' => [[
-                'allow' => true,
-                'roles' => ['@']
-            ]
 
-
-            ]
-
-
-        ]
-        ];
-    }
 
 }
